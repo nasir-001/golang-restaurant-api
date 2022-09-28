0d357d5ebdd06e8fc7000e630d09c80abf217881
Nasir Lawal <nasirlawal001@gmail.com>
2022-09-28 16:40:38 +0100

    add database file

---
+++ database/databaseConnection.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"mongodb.org/mongo-driver/mongo"
+)
+
+func DBinstance() *mongo.Client {
+	MongoDB := "mongodb://localhost:27017"
+	fmt.Print(MongoDB)
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDB))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to mongodb")
+
+	return client
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName)
+
+	return collection
+}
