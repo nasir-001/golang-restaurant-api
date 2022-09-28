2e186ce0c18b69c95365a52cabe8bfaae68e8d2c
Nasir Lawal <nasirlawal001@gmail.com>
2022-09-28 16:57:22 +0100

    add food controller function to be used

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,1 +1,38 @@
 package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"golang.org/x/text/number"
+)
+
+func GetFoods() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func GetFood() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func CreateFood() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func UpdateFood() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func round(num float64) int {
+
+}
+
+func toFixed(num float64, precision, int) float64 {
+
+}
