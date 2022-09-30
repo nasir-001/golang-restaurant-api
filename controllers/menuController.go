295f5fd46b944f0c72122e4e671c102c45c77076
Nasir Lawal <nasirlawal001@gmail.com>
2022-09-30 21:00:39 +0100

    add get all menus logic"

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -1,10 +1,33 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"golang-restuarant-management/database"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
 
 func GetMenus() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := menuCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching menus"})
+		}
 
+		var allMenus []bson.M
+		if err = result.All(ctx, &allMenus); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, result)
 	}
 }
 
