aac111e1f6ed5f3697e452c9e4c8b9cd13a8000e
Nasir Lawal <nasirlawal001@gmail.com>
2022-09-30 20:20:03 +0100

    add get food controller logic

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,9 +1,18 @@
 package controllers
 
 import (
+	"context"
+	"golang-restuarant-management/database"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/text/number"
 )
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
 func GetFoods() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
@@ -12,8 +21,17 @@ func GetFoods() gin.HandlerFunc {
 }
 
 func GetFood() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		foodId := c.Param("food_id")
+		var food models.Food
 
+		err := foodCollection.FindOne(ctx, bson.M{"food_id": foodId}).Decode(&food)
+		defer cancel()
+		if err := nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the food item"})
+		}
+		c.JSON(http.StatusOK, food)
 	}
 }
 
